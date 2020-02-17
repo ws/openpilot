eeab625e18cc5399194035db2a168b0f12a61b10
Jason Young <jyoung8607@gmail.com>
2020-02-16 19:33:34 -0500

    In-motion and brake safety check, more prep for PQ

--- panda/board/safety/safety_volkswagen.h
+++ panda/board/safety/safety_volkswagen.h
@@ -8,21 +8,25 @@ const int VOLKSWAGEN_DRIVER_TORQUE_ALLOWANCE = 80;
 const int VOLKSWAGEN_DRIVER_TORQUE_FACTOR = 3;
 
 // Safety-relevant CAN messages for the Volkswagen MQB platform
-#define MSG_EPS_01              0x09F   // RX from EPS rack, driver steering torque
-#define MSG_TSK_06              0x120   // RX from ECU, ACC status from drivetrain coordinator
-#define MSG_MOTOR_20            0x121   // RX from ECU, driver throttle input
-#define MSG_HCA_01              0x126   // TX by OP, Heading Control Assist steering torque
-#define MSG_GRA_ACC_01          0x12B   // TX by OP, ACC control buttons for cancel/resume
-#define MSG_LDW_02              0x397   // TX by OP, Lane line recognition and text alerts
+#define MSG_ESP_19      0x0B2   // RX from ABS, for wheel speeds
+#define MSG_EPS_01      0x09F   // RX from EPS, for driver steering torque
+#define MSG_ESP_05      0x106   // RX from ABS, for brake light state
+#define MSG_TSK_06      0x120   // RX from ECU, for ACC status from drivetrain coordinator
+#define MSG_MOTOR_20    0x121   // RX from ECU, for driver throttle input
+#define MSG_HCA_01      0x126   // TX by OP, Heading Control Assist steering torque
+#define MSG_GRA_ACC_01  0x12B   // TX by OP, ACC control buttons for cancel/resume
+#define MSG_LDW_02      0x397   // TX by OP, Lane line recognition and text alerts
 
 // Transmit of GRA_ACC_01 is allowed on bus 0 and 2 to keep compatibility with gateway and camera integration
 const AddrBus VOLKSWAGEN_MQB_TX_MSGS[] = {{MSG_HCA_01, 0}, {MSG_GRA_ACC_01, 0}, {MSG_GRA_ACC_01, 2}, {MSG_LDW_02, 0}};
 const int VOLKSWAGEN_MQB_TX_MSGS_LEN = sizeof(VOLKSWAGEN_MQB_TX_MSGS)/sizeof(VOLKSWAGEN_MQB_TX_MSGS[0]);
 
 AddrCheckStruct volkswagen_mqb_rx_checks[] = {
-  {.addr = {MSG_EPS_01},   .bus = 0, .check_checksum = true, .max_counter = 15U, .expected_timestep = 10000U},
-  {.addr = {MSG_TSK_06},   .bus = 0, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U},
-  {.addr = {MSG_MOTOR_20}, .bus = 0, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U},
+  {.addr = {MSG_ESP_19},   .bus = 0, .check_checksum = false, .max_counter = 0U,  .expected_timestep = 10000U},
+  {.addr = {MSG_EPS_01},   .bus = 0, .check_checksum = true,  .max_counter = 15U, .expected_timestep = 10000U},
+  {.addr = {MSG_ESP_05},   .bus = 0, .check_checksum = true,  .max_counter = 15U, .expected_timestep = 20000U},
+  {.addr = {MSG_TSK_06},   .bus = 0, .check_checksum = true,  .max_counter = 15U, .expected_timestep = 20000U},
+  {.addr = {MSG_MOTOR_20}, .bus = 0, .check_checksum = true,  .max_counter = 15U, .expected_timestep = 20000U},
 };
 const int VOLKSWAGEN_MQB_RX_CHECK_LEN = sizeof(volkswagen_mqb_rx_checks) / sizeof(volkswagen_mqb_rx_checks[0]);
 
@@ -30,7 +34,11 @@ struct sample_t volkswagen_torque_driver; // Last few driver torques measured
 int volkswagen_rt_torque_last = 0;
 int volkswagen_desired_torque_last = 0;
 uint32_t volkswagen_ts_last = 0;
+bool volkswagen_moving = false;
+bool volkswagen_brake_pressed_prev = false;
 int volkswagen_gas_prev = 0;
+int volkswagen_torque_msg = 0;
+int volkswagen_lane_msg = 0;
 uint8_t volkswagen_crc8_lut_8h2f[256]; // Static lookup table for CRC8 poly 0x2F, aka 8H2F/AUTOSAR
 
 
@@ -60,6 +68,9 @@ static uint8_t volkswagen_mqb_compute_crc(CAN_FIFOMailBox_TypeDef *to_push) {
     case MSG_EPS_01:
       crc ^= (uint8_t[]){0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5,0xF5}[counter];
       break;
+    case MSG_ESP_05:
+      crc ^= (uint8_t[]){0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07,0x07}[counter];
+      break;
     case MSG_TSK_06:
       crc ^= (uint8_t[]){0xC4,0xE2,0x4F,0xE4,0xF8,0x2F,0x56,0x81,0x9F,0xE5,0x83,0x44,0x05,0x3F,0x97,0xDF}[counter];
       break;
@@ -77,58 +88,126 @@ static uint8_t volkswagen_mqb_compute_crc(CAN_FIFOMailBox_TypeDef *to_push) {
 static void volkswagen_mqb_init(int16_t param) {
   UNUSED(param);
 
+  gen_crc_lookup_table(0x2F, volkswagen_crc8_lut_8h2f);  // CRC-8 8H2F/AUTOSAR for Volkswagen
+
   controls_allowed = false;
   relay_malfunction = false;
-  gen_crc_lookup_table(0x2F, volkswagen_crc8_lut_8h2f);  // CRC-8 8H2F/AUTOSAR for Volkswagen
+  volkswagen_torque_msg = MSG_HCA_01;
+  volkswagen_lane_msg = MSG_LDW_02;
 
   return;
 }
 
-static int volkswagen_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
+static int volkswagen_mqb_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
   bool valid = addr_safety_check(to_push, volkswagen_mqb_rx_checks, VOLKSWAGEN_MQB_RX_CHECK_LEN,
                                  volkswagen_mqb_get_crc, volkswagen_mqb_compute_crc, volkswagen_mqb_get_counter);
 
   if (valid) {
-   int bus = GET_BUS(to_push);
-   int addr = GET_ADDR(to_push);
+    int bus = GET_BUS(to_push);
+    int addr = GET_ADDR(to_push);
 
-   // Update driver input torque samples from EPS_01.Driver_Strain for absolute torque, and EPS_01.Driver_Strain_VZ
-   // for the direction
-   if ((bus == 0) && (addr == MSG_EPS_01)) {
-     int torque_driver_new = GET_BYTE(to_push, 5) | ((GET_BYTE(to_push, 6) & 0x1F) << 8);
-     int sign = (GET_BYTE(to_push, 6) & 0x80) >> 7;
-     if (sign == 1) {
-       torque_driver_new *= -1;
-     }
+    // Update in-motion state by sampling front wheel speeds
+    // Signal: ESP_19.ESP_VL_Radgeschw_02 (front left)
+    // Signal: ESP_19.ESP_VR_Radgeschw_02 (front right)
+    if ((bus == 0) && (addr == MSG_ESP_19)) {
+      int wheel_speed_fl = GET_BYTE(to_push, 4) | (GET_BYTE(to_push, 5) << 8)
+      int wheel_speed_fr = GET_BYTE(to_push, 6) | (GET_BYTE(to_push, 7) << 8)
+      if (wheel_speed_fl > 0) || (wheel_speed_fr > 0) {
+        volkswagen_moving = true;
+      } else {
+        volkswagen_moving = false;
+      }
+    }
 
-     update_sample(&volkswagen_torque_driver, torque_driver_new);
-   }
+    // Update driver input torque samples
+    // Signal: EPS_01.Driver_Strain (absolute torque)
+    // Signal: EPS_01.Driver_Strain_VZ (direction)
+    if ((bus == 0) && (addr == MSG_EPS_01)) {
+      int torque_driver_new = GET_BYTE(to_push, 5) | ((GET_BYTE(to_push, 6) & 0x1F) << 8);
+      int sign = (GET_BYTE(to_push, 6) & 0x80) >> 7;
+      if (sign == 1) {
+        torque_driver_new *= -1;
+      }
+      update_sample(&volkswagen_torque_driver, torque_driver_new);
+    }
 
-   // Monitor TSK_06.TSK_Status for the drivetrain coordinator ACC status
-   if (addr == MSG_TSK_06) {
-     int acc_status = (GET_BYTE(to_push, 3) & 0x7);
-     controls_allowed = ((acc_status == 3) || (acc_status == 4) || (acc_status == 5)) ? 1 : 0;
-   }
+    // Update ACC status from drivetrain coordinator for controls-allowed state
+    // Signal: TSK_06.TSK_Status
+    if ((bus == 0) && (addr == MSG_TSK_06)) {
+      int acc_status = (GET_BYTE(to_push, 3) & 0x7);
+      controls_allowed = ((acc_status == 3) || (acc_status == 4) || (acc_status == 5)) ? 1 : 0;
+    }
 
-   // Exit controls on rising edge of gas press, bits [12-20]
-   if (addr == MSG_MOTOR_20) {
-     int gas = (GET_BYTES_04(to_push) >> 12) & 0xFF;
-     if ((gas > 0) && (volkswagen_gas_prev == 0)) {
-       controls_allowed = 0;
-     }
-     volkswagen_gas_prev = gas;
-   }
+    // Exit controls on rising edge of gas press
+    // Signal: Motor_20.MO_Fahrpedalrohwert_01
+    if ((bus == 0) && (addr == MSG_MOTOR_20)) {
+      int gas = (GET_BYTES_04(to_push) >> 12) & 0xFF;
+      if ((gas > 0) && (volkswagen_gas_prev == 0)) {
+        controls_allowed = 0;
+      }
+      volkswagen_gas_prev = gas;
+    }
 
-   // If there are HCA messages on bus 0 not sent by OP, there's a relay problem
-   if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && (bus == 0) && (addr == MSG_HCA_01)) {
-     relay_malfunction = true;
-   }
+    // Exit controls on rising edge of brake press
+    // Signal: ESP_05.ESP_Fahrer_bremst
+    if ((bus == 0) && (addr == MSG_ESP_05)) {
+      bool brake_pressed = (GET_BYTE(to_push, 3) & 0x4) >> 2;
+      if (brake_pressed && (!(volkswagen_brake_pressed_prev) || volkswagen_moving)) {
+        controls_allowed = 0;
+      }
+      volkswagen_brake_pressed_prev = brake_pressed;
+    }
+
+    // If there are HCA messages on bus 0 not sent by OP, there's a relay problem
+    if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && (bus == 0) && (addr == MSG_HCA_01)) {
+      relay_malfunction = true;
+    }
   }
   return valid;
 }
 
-static int volkswagen_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
+static bool volkswagen_steering_valid(int desired_torque) {
+  bool violation = false;
+  uint32_t ts = TIM2->CNT;
+
+  if (controls_allowed) {
+    // *** global torque limit check ***
+    violation |= max_limit_check(desired_torque, VOLKSWAGEN_MAX_STEER, -VOLKSWAGEN_MAX_STEER);
+
+    // *** torque rate limit check ***
+    violation |= driver_limit_check(desired_torque, volkswagen_desired_torque_last, &volkswagen_torque_driver,
+      VOLKSWAGEN_MAX_STEER, VOLKSWAGEN_MAX_RATE_UP, VOLKSWAGEN_MAX_RATE_DOWN,
+      VOLKSWAGEN_DRIVER_TORQUE_ALLOWANCE, VOLKSWAGEN_DRIVER_TORQUE_FACTOR);
+    volkswagen_desired_torque_last = desired_torque;
+
+    // *** torque real time rate limit check ***
+    violation |= rt_rate_limit_check(desired_torque, volkswagen_rt_torque_last, VOLKSWAGEN_MAX_RT_DELTA);
+
+    // every RT_INTERVAL set the new limits
+    uint32_t ts_elapsed = get_ts_elapsed(ts, volkswagen_ts_last);
+    if (ts_elapsed > VOLKSWAGEN_RT_INTERVAL) {
+      volkswagen_rt_torque_last = desired_torque;
+      volkswagen_ts_last = ts;
+    }
+  }
+
+  // no torque if controls is not allowed
+  if (!controls_allowed && (desired_torque != 0)) {
+    violation = true;
+  }
+
+  // reset to 0 if either controls is not allowed or there's a violation
+  if (violation || !controls_allowed) {
+    volkswagen_desired_torque_last = 0;
+    volkswagen_rt_torque_last = 0;
+    volkswagen_ts_last = ts;
+  }
+
+  return violation;
+}
+
+static int volkswagen_mqb_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   int addr = GET_ADDR(to_send);
   int bus = GET_BUS(to_send);
   int tx = 1;
@@ -137,53 +216,17 @@ static int volkswagen_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
     tx = 0;
   }
 
-  // Safety check for HCA_01 Heading Control Assist torque.
+  // Safety check for HCA_01 Heading Control Assist torque
+  // Signal: HCA_01.Assist_Torque (absolute torque)
+  // Signal: HCA_01.Assist_VZ (direction)
   if (addr == MSG_HCA_01) {
-    bool violation = false;
-
     int desired_torque = GET_BYTE(to_send, 2) | ((GET_BYTE(to_send, 3) & 0x3F) << 8);
     int sign = (GET_BYTE(to_send, 3) & 0x80) >> 7;
     if (sign == 1) {
       desired_torque *= -1;
     }
 
-    uint32_t ts = TIM2->CNT;
-
-    if (controls_allowed) {
-
-      // *** global torque limit check ***
-      violation |= max_limit_check(desired_torque, VOLKSWAGEN_MAX_STEER, -VOLKSWAGEN_MAX_STEER);
-
-      // *** torque rate limit check ***
-      violation |= driver_limit_check(desired_torque, volkswagen_desired_torque_last, &volkswagen_torque_driver,
-        VOLKSWAGEN_MAX_STEER, VOLKSWAGEN_MAX_RATE_UP, VOLKSWAGEN_MAX_RATE_DOWN,
-        VOLKSWAGEN_DRIVER_TORQUE_ALLOWANCE, VOLKSWAGEN_DRIVER_TORQUE_FACTOR);
-      volkswagen_desired_torque_last = desired_torque;
-
-      // *** torque real time rate limit check ***
-      violation |= rt_rate_limit_check(desired_torque, volkswagen_rt_torque_last, VOLKSWAGEN_MAX_RT_DELTA);
-
-      // every RT_INTERVAL set the new limits
-      uint32_t ts_elapsed = get_ts_elapsed(ts, volkswagen_ts_last);
-      if (ts_elapsed > VOLKSWAGEN_RT_INTERVAL) {
-        volkswagen_rt_torque_last = desired_torque;
-        volkswagen_ts_last = ts;
-      }
-    }
-
-    // no torque if controls is not allowed
-    if (!controls_allowed && (desired_torque != 0)) {
-      violation = true;
-    }
-
-    // reset to 0 if either controls is not allowed or there's a violation
-    if (violation || !controls_allowed) {
-      volkswagen_desired_torque_last = 0;
-      volkswagen_rt_torque_last = 0;
-      volkswagen_ts_last = ts;
-    }
-
-    if (violation) {
+    if (volkswagen_steering_check(desired_torque)) {
       tx = 0;
     }
   }
@@ -212,7 +255,7 @@ static int volkswagen_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
         bus_fwd = 2;
         break;
       case 2:
-        if ((addr == MSG_HCA_01) || (addr == MSG_LDW_02)) {
+        if ((addr == volkswagen_torque_msg) || (addr == volkswagen_lane_msg)) {
           // OP takes control of the Heading Control Assist and Lane Departure Warning messages from the camera
           bus_fwd = -1;
         } else {
@@ -232,8 +275,8 @@ static int volkswagen_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 // Volkswagen MQB platform
 const safety_hooks volkswagen_mqb_hooks = {
   .init = volkswagen_mqb_init,
-  .rx = volkswagen_rx_hook,
-  .tx = volkswagen_tx_hook,
+  .rx = volkswagen_mqb_rx_hook,
+  .tx = volkswagen_mqb_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
   .fwd = volkswagen_fwd_hook,
   .addr_check = volkswagen_mqb_rx_checks,
